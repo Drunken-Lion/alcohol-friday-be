a7eda50c6b06252b1a68509fcc86c20932cece85
tseop <77163842+seop-kim@users.noreply.github.com>
2024-01-17 21:23:59 +0900

    project setup

---
+++ src/main/java/com/drunkenlion/alcoholfriday/AlcoholfridayApplication.java
@@ -0,0 +1,13 @@
+package com.drunkenlion.alcoholfriday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlcoholfridayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlcoholfridayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drunkenlion/alcoholfriday/AlcoholfridayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drunkenlion.alcoholfriday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlcoholfridayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
