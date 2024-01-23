4e4add152a27b81daf71931e8cc9d1135cc26ddc
dc-choi <ddagae0805@gmail.com>
2024-01-23 22:51:27 +0900

    [AF-30] feat: store 관련 entity mapping
    1. store에 관련된 entity mapping
    2. yml 설정 수정
    3. commit template 추가

---
+++ src/main/java/com/drunkenlion/alcoholfriday/domain/item/entity/ItemProduct.java
@@ -0,0 +1,26 @@
+package com.drunkenlion.alcoholfriday.domain.item.entity;
+
+import com.drunkenlion.alcoholfriday.domain.product.entity.Product;
+import com.drunkenlion.alcoholfriday.global.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@SuperBuilder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "Item_product")
+public class ItemProduct extends BaseEntity {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id", columnDefinition = "BIGINT", foreignKey = @ForeignKey(value = ConstraintMode.NO_CONSTRAINT))
+    private Item item;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", columnDefinition = "BIGINT", foreignKey = @ForeignKey(value = ConstraintMode.NO_CONSTRAINT))
+    private Product product;
+}
