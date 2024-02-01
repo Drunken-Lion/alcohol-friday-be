c29ebdd698bb82717075b63f563d50bbe18d344d
daseul-jang <devzzang94@gmail.com>
2024-02-01 22:03:55 +0900

    [af-54] feat: entity 생성 및 수정
    1. CategoryClass entity 생성
    2. Category, NcpFile, Payment entity 수정
    - Category entity의 middleName(중분류) 삭제, CategoryClass entity와 연관관계 추가
    - NcpFile entity의 테이블 이름을 명시적으로 설정, 각 필드마다 Comment 추가, columnDefinition 설정
    - Payment entity에 paymentNo(결제 번호), platform(결제 플랫폼),
      requestedAt(결제 요청일), approvedAt(결제 승인일) 필드 추가

--- src/main/java/com/drunkenlion/alcoholfriday/domain/payment/entity/Payment.java
+++ src/main/java/com/drunkenlion/alcoholfriday/domain/payment/entity/Payment.java
@@ -5,15 +5,18 @@ import com.drunkenlion.alcoholfriday.domain.order.entity.Order;
 import com.drunkenlion.alcoholfriday.global.common.enumerated.PaymentMethod;
 import com.drunkenlion.alcoholfriday.global.common.enumerated.PaymentStatus;
 import com.drunkenlion.alcoholfriday.global.common.entity.BaseEntity;
+
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+
 import org.hibernate.annotations.Comment;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -22,33 +25,53 @@ import java.math.BigDecimal;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "payment")
 public class Payment extends BaseEntity {
-    @Column(name = "status", columnDefinition = "VARCHAR(20)")
-    @Comment("결제 상태정보")
-    @Enumerated(EnumType.STRING)
-    private PaymentStatus paymentStatus;
+	@Column(name = "payment_no", columnDefinition = "VARCHAR(200)")
+	@Comment("결제 번호")
+	private String paymentNo;
 
-    @Column(name = "method", columnDefinition = "VARCHAR(20)")
-    @Comment("결제 수단")
-    @Enumerated(EnumType.STRING)
-    private PaymentMethod paymentMethod;
+	@Column(name = "status", columnDefinition = "VARCHAR(20)")
+	@Comment("결제 상태정보")
+	@Enumerated(EnumType.STRING)
+	private PaymentStatus paymentStatus;
 
-    @Column(name = "order_price", columnDefinition = "DECIMAL(64, 3)")
-    @Comment("배송 금액")
-    private BigDecimal orderPrice;
+	@Column(name = "method", columnDefinition = "VARCHAR(20)")
+	@Comment("결제 수단")
+	@Enumerated(EnumType.STRING)
+	private PaymentMethod paymentMethod;
 
-    @Column(name = "delivery_price", columnDefinition = "DECIMAL(64, 3)")
-    @Comment("배송 금액")
-    private BigDecimal deliveryPrice;
+	@Column(name = "order_price", columnDefinition = "DECIMAL(64, 3)")
+	@Comment("배송 금액")
+	private BigDecimal orderPrice;
 
-    @Column(name = "total_price", columnDefinition = "DECIMAL(64, 3)")
-    @Comment("결제 총 금액")
-    private BigDecimal totalPrice;
+	@Column(name = "delivery_price", columnDefinition = "DECIMAL(64, 3)")
+	@Comment("배송 금액")
+	private BigDecimal deliveryPrice;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "order_id", columnDefinition = "BIGINT", foreignKey = @ForeignKey(value = ConstraintMode.NO_CONSTRAINT))
-    private Order order;
+	@Column(name = "total_price", columnDefinition = "DECIMAL(64, 3)")
+	@Comment("결제 총 금액")
+	private BigDecimal totalPrice;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id", columnDefinition = "BIGINT", foreignKey = @ForeignKey(value = ConstraintMode.NO_CONSTRAINT))
-    private Member member;
+	@Column(name = "platform", columnDefinition = "VARCHAR(20)")
+	@Comment("결제 플랫폼")
+	private String platform;
+
+	@Column(name = "requested_at", columnDefinition = "DATETIME")
+	@Comment("결제 요청일")
+	private LocalDateTime requestedAt;
+
+	@Column(name = "approved_at", columnDefinition = "DATETIME")
+	@Comment("결제 승인일")
+	private LocalDateTime approvedAt;
+
+	@Column(name = "currency", columnDefinition = "VARCHAR(20)")
+	@Comment("결제 통화")
+	private String currency;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "order_id", columnDefinition = "BIGINT", foreignKey = @ForeignKey(value = ConstraintMode.NO_CONSTRAINT))
+	private Order order;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id", columnDefinition = "BIGINT", foreignKey = @ForeignKey(value = ConstraintMode.NO_CONSTRAINT))
+	private Member member;
 }
