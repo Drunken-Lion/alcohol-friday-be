5497a876665796bdb7a73f88396e2c46cf986fbd
ChaChaMong <thflf93@gmail.com>
2024-02-02 22:19:14 +0900

    [AF-57] feat: admin - 전체 회원 조회 기능 구현
    관리자 회원관리화면의 전체 회원 조회 기능을 추가하였습니다.
    Controller, Service Test code도 추가 하였습니다.

--- src/main/java/com/drunkenlion/alcoholfriday/domain/admin/api/AdminController.java
+++ src/main/java/com/drunkenlion/alcoholfriday/domain/admin/api/AdminController.java
@@ -2,7 +2,11 @@ package com.drunkenlion.alcoholfriday.domain.admin.api;
 
 import com.drunkenlion.alcoholfriday.domain.admin.application.AdminCustomerService;
 import com.drunkenlion.alcoholfriday.domain.admin.application.AdminMemberService;
+import com.drunkenlion.alcoholfriday.domain.admin.dto.MemberListResponse;
+import com.drunkenlion.alcoholfriday.global.common.response.PageResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdminController {
     private final AdminMemberService adminMemberService;
     private final AdminCustomerService adminCustomerService;
+
+    @GetMapping(value = "members")
+    public ResponseEntity<PageResponse<MemberListResponse>> getMembers() {
+        PageResponse<MemberListResponse> pageResponse = PageResponse.of(this.adminMemberService.getMembers());
+        return ResponseEntity.ok().body(pageResponse);
+    }
 }
---
+++ src/main/java/com/drunkenlion/alcoholfriday/domain/admin/dto/MemberListResponse.java
@@ -0,0 +1,32 @@
+package com.drunkenlion.alcoholfriday.domain.admin.dto;
+
+import com.drunkenlion.alcoholfriday.domain.member.entity.Member;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+public class MemberListResponse {
+    private Long id;
+    private String name;
+    private String nickname;
+    private String email;
+    private String role;
+    private LocalDateTime createdAt;
+    private boolean deleted;
+
+    public static MemberListResponse of(Member member) {
+        return MemberListResponse.builder()
+                .id(member.getId())
+                .name(member.getName())
+                .nickname(member.getNickname())
+                .email(member.getEmail())
+                .role(member.getRole())
+                .createdAt(member.getCreatedAt())
+                .deleted(member.getDeletedAt() != null)
+                .build();
+    }
+}
