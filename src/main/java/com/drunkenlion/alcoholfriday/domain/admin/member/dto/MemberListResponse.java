db92d0764627b81d147c22de05b5d3a8ebdcaa76
ChaChaMong <thflf93@gmail.com>
2024-02-08 21:12:38 +0900

    [af-80] fix: 누락된 swagger schema 세팅 복원

--- src/main/java/com/drunkenlion/alcoholfriday/domain/admin/member/dto/MemberListResponse.java
+++ src/main/java/com/drunkenlion/alcoholfriday/domain/admin/member/dto/MemberListResponse.java
@@ -1,21 +1,38 @@
 package com.drunkenlion.alcoholfriday.domain.admin.member.dto;
 
 import com.drunkenlion.alcoholfriday.domain.member.entity.Member;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
 import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Getter
+@Builder
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Builder
+@Schema(description = "전체 회원 조회 항목")
 public class MemberListResponse {
+    @Schema(description = "고유 아이디")
     private Long id;
+
+    @Schema(description = "이름")
     private String name;
+
+    @Schema(description = "별명")
     private String nickname;
+
+    @Email
+    @Schema(description = "이메일", example = "example@example.com")
     private String email;
+
+    @Schema(description = "권한")
     private String role;
+
+    @Schema(description = "생성일시")
     private LocalDateTime createdAt;
+
+    @Schema(description = "삭제여부")
     private boolean deleted;
 
     public static MemberListResponse of(Member member) {
