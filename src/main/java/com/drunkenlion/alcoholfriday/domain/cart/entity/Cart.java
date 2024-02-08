3db68eb4b86cc93d8ff09b3662f3d1242ddd32fe
Son-Gyeongi <thsruddl77@gmail.com>
2024-02-08 20:58:34 +0900

    [AF-61] test: Cart 저장 기능 test 진행중

--- src/main/java/com/drunkenlion/alcoholfriday/domain/cart/entity/Cart.java
+++ src/main/java/com/drunkenlion/alcoholfriday/domain/cart/entity/Cart.java
@@ -2,20 +2,19 @@ package com.drunkenlion.alcoholfriday.domain.cart.entity;
 
 import com.drunkenlion.alcoholfriday.domain.member.entity.Member;
 import com.drunkenlion.alcoholfriday.global.common.entity.BaseEntity;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
-import java.util.ArrayList;
-import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.Comment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
