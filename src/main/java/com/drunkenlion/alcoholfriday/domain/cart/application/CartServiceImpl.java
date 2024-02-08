afb3084c9495640d358325807f99219d2f90bb52
Son-Gyeongi <thsruddl77@gmail.com>
2024-02-08 23:18:52 +0900

    [AF-62] feat: Cart 삭제 기능 구현

---
+++ src/main/java/com/drunkenlion/alcoholfriday/domain/cart/application/CartServiceImpl.java
@@ -0,0 +1,39 @@
+package com.drunkenlion.alcoholfriday.domain.cart.application;
+
+import com.drunkenlion.alcoholfriday.domain.cart.dao.CartDetailRepository;
+import com.drunkenlion.alcoholfriday.domain.cart.dao.CartRepository;
+import com.drunkenlion.alcoholfriday.domain.cart.dto.DeleteCartRequest;
+import com.drunkenlion.alcoholfriday.domain.cart.entity.Cart;
+import com.drunkenlion.alcoholfriday.domain.member.entity.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class CartServiceImpl implements CartService {
+    private final CartRepository cartRepository;
+    private final CartDetailRepository cartDetailRepository;
+
+    @Override
+    public void deleteCart(List<DeleteCartRequest> cartRequests, Member member) {
+        Cart cart = addFirstCart(member);
+
+        if (cart == null) throw new IllegalArgumentException("현재 장바구니에 상품이 없습니다.");
+
+        cartRequests.forEach(cartRequest -> deleteCart(cartRequest, cart));
+    }
+
+    @Override
+    public void deleteCart(DeleteCartRequest cartRequest, Cart cart) {
+        cartDetailRepository.deleteByIdAndCart(cartRequest.getItemId(), cart);
+    }
+
+    @Override
+    public Cart addFirstCart(Member member) {
+        return cartRepository.findFirstByMember(member).orElse(null);
+    }
+}
