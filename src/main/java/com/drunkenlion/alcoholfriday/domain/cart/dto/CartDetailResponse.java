24d07d7c27aa0ef5bef37ed7e06653a7964fa03a
Son-Gyeongi <thsruddl77@gmail.com>
2024-02-08 23:18:52 +0900

    [AF-62] feat: Cart 삭제 기능 구현

---
+++ src/main/java/com/drunkenlion/alcoholfriday/domain/cart/dto/CartDetailResponse.java
@@ -0,0 +1,21 @@
+package com.drunkenlion.alcoholfriday.domain.cart.dto;
+
+import com.drunkenlion.alcoholfriday.domain.cart.entity.CartDetail;
+import com.drunkenlion.alcoholfriday.domain.item.dto.FindItemResponse;
+import lombok.*;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+public class CartDetailResponse {
+    private FindItemResponse item;
+    private Long quantity;
+
+    public static CartDetailResponse of(CartDetail cartDetail) {
+        return CartDetailResponse.builder()
+                .item(FindItemResponse.of(cartDetail.getItem()))
+                .quantity(cartDetail.getQuantity())
+                .build();
+    }
+}
