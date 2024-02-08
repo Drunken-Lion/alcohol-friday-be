9f84c434c3254da731ec4f37a2173d13bf79a994
Son-Gyeongi <thsruddl77@gmail.com>
2024-02-08 21:58:09 +0900

    [AF-60] test: Cart 조회 기능 진행중

---
+++ src/test/java/com/drunkenlion/alcoholfriday/domain/cart/application/CartServiceTest.java
@@ -0,0 +1,23 @@
+package com.drunkenlion.alcoholfriday.domain.cart.application;
+
+import com.drunkenlion.alcoholfriday.domain.cart.dao.CartDetailRepository;
+import com.drunkenlion.alcoholfriday.domain.cart.dao.CartRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+@ExtendWith(MockitoExtension.class)
+@Transactional
+class CartServiceTest {
+    @InjectMocks
+    private CartServiceImpl cartService;
+    @Mock
+    private CartRepository cartRepository;
+    @Mock
+    private CartDetailRepository cartDetailRepository;
+
+    // test를 위한 임의 변수
+
+}
