af2bba0936905aab33341956d81e67f84de33921
Son-Gyeongi <thsruddl77@gmail.com>
2024-02-09 12:53:41 +0900

    [AF-60] test: Cart 조회 기능 진행중 - 장바구니에 한 개 상품 담았을 경우 완료 - 회원에게 카트가 없는 경우 완료

--- src/main/java/com/drunkenlion/alcoholfriday/domain/cart/dto/CartDetailResponse.java
+++ src/main/java/com/drunkenlion/alcoholfriday/domain/cart/dto/CartDetailResponse.java
@@ -1,8 +1,11 @@
 package com.drunkenlion.alcoholfriday.domain.cart.dto;
 
+import com.drunkenlion.alcoholfriday.domain.cart.entity.Cart;
 import com.drunkenlion.alcoholfriday.domain.cart.entity.CartDetail;
 import com.drunkenlion.alcoholfriday.domain.item.dto.FindItemResponse;
 import lombok.*;
+
+import java.math.BigDecimal;
 
 @Getter
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
@@ -11,11 +14,13 @@ import lombok.*;
 public class CartDetailResponse {
     private FindItemResponse item;
     private Long quantity;
+    private BigDecimal totalCartPrice;
 
-    public static CartDetailResponse of(CartDetail cartDetail) {
+    public static CartDetailResponse of(CartDetail cartDetail, Cart cart) {
         return CartDetailResponse.builder()
                 .item(FindItemResponse.of(cartDetail.getItem()))
                 .quantity(cartDetail.getQuantity())
+                .totalCartPrice(cart.getTotalCartPrice())
                 .build();
     }
 }
