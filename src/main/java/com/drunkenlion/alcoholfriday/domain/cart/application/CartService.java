476a74b86c562962b9f825bcfb5295fea4e17238
Son-Gyeongi <thsruddl77@gmail.com>
2024-02-10 21:49:17 +0900

    [AF-60] test: Cart 조회 기능 테스트 완료 - 장바구니에 한 개 상품 담았을 경우 완료 - 장바구니에 한 개 이상 상품 담았을 경우 - 카트에 아무런 상품이 없는 경우에 대한 처리

--- src/main/java/com/drunkenlion/alcoholfriday/domain/cart/application/CartService.java
+++ src/main/java/com/drunkenlion/alcoholfriday/domain/cart/application/CartService.java
@@ -1,10 +1,11 @@
 package com.drunkenlion.alcoholfriday.domain.cart.application;
 
-import com.drunkenlion.alcoholfriday.domain.cart.dto.CartDetailResponse;
+import com.drunkenlion.alcoholfriday.domain.cart.dto.CartResponse;
+import com.drunkenlion.alcoholfriday.domain.cart.entity.Cart;
 import com.drunkenlion.alcoholfriday.domain.member.entity.Member;
 
-import java.util.List;
-
 public interface CartService {
-    List<CartDetailResponse> getCartList(Member member);
+    CartResponse getCartList(Member member);
+
+    Cart addFirstCart(Member member);
 }
