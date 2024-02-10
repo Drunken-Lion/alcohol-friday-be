c4ea889f134e27d7a8873780fc52e5e6b7185a05
Son-Gyeongi <thsruddl77@gmail.com>
2024-02-10 21:49:17 +0900

    [AF-60] test: Cart 조회 기능 테스트 완료 - 장바구니에 한 개 상품 담았을 경우 완료 - 장바구니에 한 개 이상 상품 담았을 경우 - 카트에 아무런 상품이 없는 경우에 대한 처리

--- src/test/java/com/drunkenlion/alcoholfriday/domain/cart/application/CartServiceTest.java
+++ src/test/java/com/drunkenlion/alcoholfriday/domain/cart/application/CartServiceTest.java
@@ -2,7 +2,7 @@ package com.drunkenlion.alcoholfriday.domain.cart.application;
 
 import com.drunkenlion.alcoholfriday.domain.cart.dao.CartDetailRepository;
 import com.drunkenlion.alcoholfriday.domain.cart.dao.CartRepository;
-import com.drunkenlion.alcoholfriday.domain.cart.dto.CartDetailResponse;
+import com.drunkenlion.alcoholfriday.domain.cart.dto.CartResponse;
 import com.drunkenlion.alcoholfriday.domain.cart.entity.Cart;
 import com.drunkenlion.alcoholfriday.domain.cart.entity.CartDetail;
 import com.drunkenlion.alcoholfriday.domain.category.entity.Category;
@@ -60,6 +60,24 @@ class CartServiceTest {
     private final Long insense = 10L;
     private final Long throat = 10L;
 
+    // Item2
+    private final String firstName2 = "식품";
+    private final String lastName2 = "청주";
+    private final String productName2 = "test data2";
+    private final String itemName2 = "test ddaattaa";
+    private final BigDecimal price2 = new BigDecimal(100_000);
+    private final String info2 = "이 상품은 테스트 상품2 입니다.";
+    private final Long quantity2 = 10L;
+    private final Long alcohol2 = 17L;
+    private final String ingredient2 = "알콜, 누룩 등등...";
+    private final Long sweet2 = 10L;
+    private final Long sour2 = 10L;
+    private final Long cool2 = 10L;
+    private final Long body2 = 10L;
+    private final Long balence2 = 10L;
+    private final Long insense2 = 10L;
+    private final Long throat2 = 10L;
+
     // Member
     private final Long id = 1L;
     private final String email = "test@example.com";
@@ -84,33 +102,62 @@ class CartServiceTest {
     // CartDetail
     private final Cart cart = getDataCart();
     private final Item item = getDataItem();
+    private final Item item2 = getDataItem2();
     private final Long quantityCart = 2L;
+    private final Long quantityCart2 = 1L;
 
 
     @Test
     @DisplayName("장바구니에 한 개 상품 담았을 경우")
-    void getCartListTest() {
+    void getCartDetailOneTest() {
         // given
-        when(this.cartRepository.findByMember(any(Member.class))).thenReturn(this.getOneCart());
+        when(this.cartRepository.findFirstByMember(any(Member.class))).thenReturn(this.getOneCart());
 
         List<CartDetail> cartDetails = new ArrayList<>();
         cartDetails.add(getDataCartDetail());
         when(this.cartDetailRepository.findAllByCart(any(Cart.class))).thenReturn(cartDetails);
 
         // when
-        List<CartDetailResponse> cartList = this.cartService.getCartList(getDataMember());
+        CartResponse cartList = this.cartService.getCartList(getDataMember());
 
         // then
-        assertThat(cartList.get(0).getQuantity()).isEqualTo(2L);
-        assertThat(cartList.get(0).getItem().getName()).isEqualTo(itemName);
-        assertThat(cartList.get(0).getItem().getPrice()).isEqualTo(price);
+        assertThat(cartList.getCartDetails().get(0).getQuantity()).isEqualTo(2L);
+        assertThat(cartList.getCartDetails().get(0).getItem().getName()).isEqualTo(itemName);
+        assertThat(cartList.getCartDetails().get(0).getItem().getPrice()).isEqualTo(price);
+        assertThat(cartList.getTotalCartPrice()).isEqualTo(new BigDecimal("50000"));
+        assertThat(cartList.getTotalCartQuantity()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("장바구니에 한 개 이상 상품 담았을 경우")
+    void getCartListTest() {
+        // given
+        when(this.cartRepository.findFirstByMember(any(Member.class))).thenReturn(this.getOneCart());
+
+        List<CartDetail> cartDetails = new ArrayList<>();
+        cartDetails.add(getDataCartDetail());
+        cartDetails.add(getDataCartDetail2());
+        when(this.cartDetailRepository.findAllByCart(any(Cart.class))).thenReturn(cartDetails);
+
+        // when
+        CartResponse cartList = this.cartService.getCartList(getDataMember());
+
+        // then
+        assertThat(cartList.getCartDetails().get(0).getQuantity()).isEqualTo(2L);
+        assertThat(cartList.getCartDetails().get(0).getItem().getName()).isEqualTo(itemName);
+        assertThat(cartList.getCartDetails().get(0).getItem().getPrice()).isEqualTo(price);
+        assertThat(cartList.getCartDetails().get(1).getQuantity()).isEqualTo(1L);
+        assertThat(cartList.getCartDetails().get(1).getItem().getName()).isEqualTo(itemName2);
+        assertThat(cartList.getCartDetails().get(1).getItem().getPrice()).isEqualTo(price2);
+        assertThat(cartList.getTotalCartPrice()).isEqualTo(new BigDecimal("150000"));
+        assertThat(cartList.getTotalCartQuantity()).isEqualTo(3);
     }
 
     @Test
     @DisplayName("회원에게 카트가 없는 경우")
     void getCartList_EmptyCart() {
         // given
-        when(cartRepository.findByMember(any(Member.class))).thenReturn(Optional.empty());
+        when(cartRepository.findFirstByMember(any(Member.class))).thenReturn(Optional.empty());
 
         // when & then
         assertThrows(IllegalArgumentException.class, () -> {
@@ -118,8 +165,20 @@ class CartServiceTest {
         });
     }
 
-    // 카트에 아무런 상품이 없는 경우에 대한 처리
-    // 여러 개의 카트 및 카트 디테일이 있는 경우에 대한 처리
+    @Test
+    @DisplayName("카트에 아무런 상품이 없는 경우")
+    void getCartList_EmptyCartDetail() {
+        // given
+        when(cartRepository.findFirstByMember(any(Member.class))).thenReturn(this.getOneCart());
+
+        List<CartDetail> cartDetails = new ArrayList<>();
+        when(this.cartDetailRepository.findAllByCart(any(Cart.class))).thenReturn(cartDetails);
+
+        // when & then
+        assertThrows(IllegalArgumentException.class, () -> {
+            cartService.getCartList(getDataMember());
+        });
+    }
 
 
     private Optional<Cart> getOneCart() {
@@ -141,6 +200,18 @@ class CartServiceTest {
                 .cart(cart)
                 .item(item)
                 .quantity(quantityCart)
+                .build();
+    }
+
+    private Optional<CartDetail> getOneCartDetail2() {
+        return Optional.of(this.getDataCartDetail2());
+    }
+
+    private CartDetail getDataCartDetail2() {
+        return CartDetail.builder()
+                .cart(cart)
+                .item(item2)
+                .quantity(quantityCart2)
                 .build();
     }
 
@@ -200,6 +271,52 @@ class CartServiceTest {
                 .name(itemName)
                 .price(price)
                 .info(info)
+                .build();
+        item.addCategory(category);
+
+        ItemProduct itemProduct = ItemProduct.builder()
+                .item(item)
+                .product(product)
+                .build();
+        itemProduct.addItem(item);
+        itemProduct.addProduct(product);
+
+        return item;
+    }
+
+    private Optional<Item> getOneItem2() {
+        return Optional.of(this.getDataItem2());
+    }
+
+    private Item getDataItem2() {
+        CategoryClass categoryClass = CategoryClass.builder()
+                .firstName(firstName2)
+                .build();
+
+        Category category = Category.builder()
+                .lastName(lastName2)
+                .build();
+        category.addCategoryClass(categoryClass);
+
+        Product product = Product.builder()
+                .name(productName2)
+                .quantity(quantity2)
+                .alcohol(alcohol2)
+                .ingredient(ingredient2)
+                .sweet(sweet2)
+                .sour(sour2)
+                .cool(cool2)
+                .body(body2)
+                .balence(balence2)
+                .insense(insense2)
+                .throat(throat2)
+                .build();
+        product.addCategory(category);
+
+        Item item = Item.builder()
+                .name(itemName2)
+                .price(price2)
+                .info(info2)
                 .build();
         item.addCategory(category);
 
