42db5db9d670257401f95e25e381d0ed672119d0
Son-Gyeongi <thsruddl77@gmail.com>
2024-02-12 21:00:57 +0900

    [AF-62] test: 장바구니에 상품이 없는 경우 테스트 완료(delete 기능)

--- src/main/java/com/drunkenlion/alcoholfriday/domain/cart/entity/Cart.java
+++ src/main/java/com/drunkenlion/alcoholfriday/domain/cart/entity/Cart.java
@@ -2,20 +2,16 @@ package com.drunkenlion.alcoholfriday.domain.cart.entity;
 
 import com.drunkenlion.alcoholfriday.domain.member.entity.Member;
 import com.drunkenlion.alcoholfriday.global.common.entity.BaseEntity;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
-import java.util.ArrayList;
-import java.util.List;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.Comment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -27,6 +23,7 @@ public class Cart extends BaseEntity {
     @OneToOne(fetch = FetchType.LAZY)
     private Member member;
 
+    @Builder.Default
     @Comment("장바구니에 담긴 상품")
     @OneToMany(mappedBy = "cart")
     private List<CartDetail> cartDetails = new ArrayList<>();
