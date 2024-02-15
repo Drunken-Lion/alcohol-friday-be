c5507e2975b5dfffe36641571d135dd96fd3124f
Son-Gyeongi <thsruddl77@gmail.com>
2024-02-15 18:21:30 +0900

    [AF-60] test: 장바구니 조회 API test 진행중

--- src/test/java/com/drunkenlion/alcoholfriday/domain/cart/api/CartControllerTest.java
+++ src/test/java/com/drunkenlion/alcoholfriday/domain/cart/api/CartControllerTest.java
@@ -1,14 +1,41 @@
 package com.drunkenlion.alcoholfriday.domain.cart.api;
 
+import com.drunkenlion.alcoholfriday.domain.auth.enumerated.ProviderType;
 import com.drunkenlion.alcoholfriday.domain.cart.dao.CartDetailRepository;
 import com.drunkenlion.alcoholfriday.domain.cart.dao.CartRepository;
+import com.drunkenlion.alcoholfriday.domain.category.dao.CategoryClassRepository;
+import com.drunkenlion.alcoholfriday.domain.category.dao.CategoryRepository;
+import com.drunkenlion.alcoholfriday.domain.category.entity.Category;
+import com.drunkenlion.alcoholfriday.domain.category.entity.CategoryClass;
+import com.drunkenlion.alcoholfriday.domain.item.dao.ItemProductRepository;
+import com.drunkenlion.alcoholfriday.domain.item.dao.ItemRepository;
+import com.drunkenlion.alcoholfriday.domain.item.entity.Item;
+import com.drunkenlion.alcoholfriday.domain.item.entity.ItemProduct;
+import com.drunkenlion.alcoholfriday.domain.member.dao.MemberRepository;
+import com.drunkenlion.alcoholfriday.domain.member.entity.Member;
+import com.drunkenlion.alcoholfriday.domain.member.enumerated.MemberRole;
+import com.drunkenlion.alcoholfriday.domain.product.dao.ProductRepository;
+import com.drunkenlion.alcoholfriday.domain.product.entity.Product;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -16,9 +43,120 @@ import static org.junit.jupiter.api.Assertions.*;
 class CartControllerTest {
     @Autowired
     private MockMvc mvc;
+
     @Autowired
     private CartRepository cartRepository;
     @Autowired
     private CartDetailRepository cartDetailRepository;
+    @Autowired
+    private MemberRepository memberRepository;
+    @Autowired
+    private ItemRepository itemRepository;
+    @Autowired
+    private ItemProductRepository itemProductRepository;
+    @Autowired
+    private ProductRepository productRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
+    @Autowired
+    private CategoryClassRepository categoryClassRepository;
 
+    @BeforeEach
+    @Transactional
+    void beforeEach() {
+        // Item
+        CategoryClass categoryClass = CategoryClass.builder()
+                .firstName("식품")
+                .build();
+
+        Category category = Category.builder()
+                .lastName("탁주")
+                .build();
+        category.addCategoryClass(categoryClass);
+
+        Product product = Product.builder()
+                .name("test data")
+                .quantity(10L)
+                .alcohol(17L)
+                .ingredient("알콜, 누룩 등등...")
+                .sweet(1L)
+                .sour(1L)
+                .cool(1L)
+                .body(1L)
+                .balence(1L)
+                .insense(1L)
+                .throat(1L)
+                .build();
+        product.addCategory(category);
+
+        Item item = Item.builder()
+                .name("test ddaattaa")
+                .price(new BigDecimal(50000))
+                .info("이 상품은 테스트 상품입니다.")
+                .build();
+        item.addCategory(category);
+
+        ItemProduct itemProduct = ItemProduct.builder()
+                .item(item)
+                .product(product)
+                .build();
+        itemProduct.addItem(item);
+        itemProduct.addProduct(product);
+
+        categoryClassRepository.save(categoryClass);
+        categoryRepository.save(category);
+        productRepository.save(product);
+        itemRepository.save(item);
+        itemProductRepository.save(itemProduct);
+
+        // Member
+        Member member = Member.builder()
+                .email("test@example.com")
+                .provider(ProviderType.KAKAO)
+                .name("테스트")
+                .nickname("test")
+                .role(MemberRole.MEMBER)
+                .phone(1012345678L)
+                .certifyAt(null)
+                .agreedToServiceUse(true)
+                .agreedToServicePolicy(true)
+                .agreedToServicePolicyUse(true)
+                .createdAt(LocalDateTime.now())
+                .updatedAt(null)
+                .deletedAt(null)
+                .build();
+
+        memberRepository.save(member);
+    }
+
+    @AfterEach
+    @Transactional
+    void afterEach() {
+        categoryClassRepository.deleteAll();
+        categoryRepository.deleteAll();
+        productRepository.deleteAll();
+        itemRepository.deleteAll();
+        itemProductRepository.deleteAll();
+        memberRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("장바구니 조회")
+    @WithUserDetails("user")
+    void getCartList() throws Exception {
+        // when
+        ResultActions resultActions = mvc
+                .perform(get("/v1/carts/list")
+                )
+                .andDo(print());
+
+        // then
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(handler().handlerType(CartController.class))
+                .andExpect(handler().methodName("getCartList"))
+                .andExpect(jsonPath("$.cartDetails", nullValue()))
+                .andExpect(jsonPath("$.totalCartPrice", notNullValue()))
+                .andExpect(jsonPath("$.totalCartQuantity", notNullValue()));
+    }
 }
