9875d9536ec84842c935a645b5156583fc2fc0e7
tseop <77163842+seop-kim@users.noreply.github.com>
2024-02-20 01:06:04 +0900

    [af-19] refactor: Deprecated 메서드 복원 및 주석 추가

--- src/main/java/com/drunkenlion/alcoholfriday/domain/cart/entity/Cart.java
+++ src/main/java/com/drunkenlion/alcoholfriday/domain/cart/entity/Cart.java
@@ -27,4 +27,20 @@ public class Cart extends BaseEntity {
     @Comment("장바구니에 담긴 상품(들)")
     @OneToMany(mappedBy = "cart")
     private List<CartDetail> cartDetails = new ArrayList<>();
+
+    /**
+     * @deprecated Builder로 객체가 생성되도록 하기 위해 삭제될 예정입니다.
+     */
+    @Deprecated
+    // Member가 첫 장바구니 관련 기능을 사용할 때 추가해 주시면 됩니다.
+    public static Cart create(Member member) {
+        Cart cart = new Cart();
+        cart.createCart(member);
+        return cart;
+    }
+
+    @Deprecated
+    private void createCart(Member member) {
+        this.member = member;
+    }
 }
