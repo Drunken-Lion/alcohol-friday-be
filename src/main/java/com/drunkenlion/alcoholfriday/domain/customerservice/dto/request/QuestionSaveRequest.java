0979ca16e1d16270a95cdcf3e2a4bc20b6c5f898
tseop <77163842+seop-kim@users.noreply.github.com>
2024-02-21 14:59:32 +0900

    [af-108] refactor: QuestionRequest 네이밍 수정 및 Valid 설정

--- src/main/java/com/drunkenlion/alcoholfriday/domain/customerservice/dto/request/QuestionSaveRequest.java
+++ src/main/java/com/drunkenlion/alcoholfriday/domain/customerservice/dto/request/QuestionSaveRequest.java
@@ -3,30 +3,28 @@ package com.drunkenlion.alcoholfriday.domain.customerservice.dto.request;
 import com.drunkenlion.alcoholfriday.domain.customerservice.entity.Question;
 import com.drunkenlion.alcoholfriday.domain.member.entity.Member;
 import io.swagger.v3.oas.annotations.media.Schema;
-import java.util.List;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Schema(description = "문의사항 등록 요청 항목")
-public class QuestionRequest {
+public class QuestionSaveRequest {
+    @NotBlank(message = "문의의 제목이 존재하지 않습니다.")
     @Schema(description = "문의사항 제목")
     private String title;
 
+    @NotBlank(message = "문의의 내용이 존재하지 않습니다.")
     @Schema(description = "문의사항 내용")
     private String content;
 
-    @Schema(description = "문의사항 등록 이미지")
-    private List<MultipartFile> files;
-
-    public static Question toEntity(QuestionRequest request, Member member) {
+    public static Question toEntity(QuestionSaveRequest request, Member member) {
         return Question.builder()
                 .title(request.getTitle())
                 .content(request.getContent())
