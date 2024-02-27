0a2417b0b80613a701a8b5a5fb57a9d7ad4268fa
ChaChaMong <thflf93@gmail.com>
2024-02-28 01:48:00 +0900

    [af-119] feat: admin - 스토어 주문 관련 기능 구현
    - 주문 전체 목록 조회 기능
    - 주문 상태 선택 목록 조회 기능
    - 주문 상세 조회 기능
    - 주문 수정

---
+++ src/main/java/com/drunkenlion/alcoholfriday/domain/admin/order/dto/OrderListResponse.java
@@ -0,0 +1,48 @@
+package com.drunkenlion.alcoholfriday.domain.admin.order.dto;
+
+import com.drunkenlion.alcoholfriday.domain.order.entity.Order;
+import com.drunkenlion.alcoholfriday.global.common.enumerated.OrderStatus;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Schema(description = "전체 주문 조회 항목")
+public class OrderListResponse {
+    @Schema(description = "고유 아이디")
+    private Long id;
+
+    @Schema(description = "주문번호")
+    private String orderNo;
+
+    @Schema(description = "주문자 이름")
+    private String customerName;
+
+    @Schema(description = "주문자 별명")
+    private String customerNickname;
+
+    @Schema(description = "주문 상태")
+    private OrderStatus orderStatus;
+
+    @Schema(description = "주문 총 금액")
+    private BigDecimal price;
+
+    // TODO: 결제 플랫폼 추가 필요
+//    @Schema(description = "결제 플랫폼")
+//    private String platform;
+
+    public static OrderListResponse of(Order order) {
+        return OrderListResponse.builder()
+                .id(order.getId())
+                .orderNo(order.getOrderNo())
+                .customerName(order.getMember().getName())
+                .customerNickname(order.getMember().getNickname())
+                .orderStatus(order.getOrderStatus())
+                .price(order.getPrice())
+                .build();
+    }
+}
