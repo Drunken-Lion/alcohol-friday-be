1166557ba9950a1482e49dbbfb57f20a125a124a
dc-choi <ddagae0805@gmail.com>
2024-02-28 22:16:41 +0900

    [af-159] build: modify build script
    빌드 스트립트에 수정사항이 있었습니다.

--- deploy-server.sh
+++ deploy-server.sh
@@ -2,18 +2,21 @@
 # GitHub에 저장하기 위한 용도, 스크립트는 서버에 따로 올려둔다.
 
 ### Check Argument
-if [ $# -ne 5 ]; then
+if [ $# -ne 4 ]; then
 	echo 'Check arguments'
 	exit -1
 fi
 
+URL=alcohol-friday-docker-image.kr.ncr.ntruss.com
 IMAGE_NAME="alcohol-friday"
 
-docker login -u $1 -p $2 $3
+docker login -u $1 -p $2 $URL
 docker pull ngnix
-docker pull $3/$IMAGE_NAME:$4
-docker tag $3/$IMAGE_NAME:$4 $IMAGE_NAME:$5
+docker pull $URL/$IMAGE_NAME:$3
+docker tag $URL/$IMAGE_NAME:$3 $IMAGE_NAME:$4
 
+docker compose down
 docker compose -p $IMAGE_NAME up -d
+docker compose ps
 
 docker image prune -a -f
