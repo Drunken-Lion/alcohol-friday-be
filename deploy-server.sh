56e9cc78f7af8b843e13fb9f20da93755302c7ae
dc-choi <ddagae0805@gmail.com>
2024-02-28 23:17:56 +0900

    [af-159] build

--- deploy-server.sh
+++ deploy-server.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
-# GitHub에 저장하기 위한 용도, 스크립트는 서버에 따로 올려둔다.
 
-### Check Argument
 if [ $# -ne 4 ]; then
-	echo 'Check arguments'
-	exit -1
+  echo 'Check arguments'
+  exit -1
 fi
 
-URL=alcohol-friday-docker-image.kr.ncr.ntruss.com
+URL="alcohol-friday-docker-image.kr.ncr.ntruss.com"
 IMAGE_NAME="alcohol-friday"
 
+docker pull nginx
+
 docker login -u $1 -p $2 $URL
-docker pull ngnix
 docker pull $URL/$IMAGE_NAME:$3
-docker tag $URL/$IMAGE_NAME:$3 $IMAGE_NAME:$4
+docker tag $IMAGE_NAME:$4 $URL/$IMAGE_NAME:$3
 
 docker compose down
 docker compose -p $IMAGE_NAME up -d
