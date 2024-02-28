60bb6c75671114fe98ced9fdb748671e9666595f
dc-choi <ddagae0805@gmail.com>
2024-02-28 23:46:58 +0900

    [af-159] build

--- deploy-server.sh
+++ deploy-server.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 if [ $# -ne 4 ]; then
-  echo 'Check arguments'
-  exit -1
+        echo 'Check arguments'
+        exit -1
 fi
 
 URL="alcohol-friday-docker-image.kr.ncr.ntruss.com"
@@ -12,10 +12,12 @@ docker pull nginx
 
 docker login -u $1 -p $2 $URL
 docker pull $URL/$IMAGE_NAME:$3
-docker tag $IMAGE_NAME:$4 $URL/$IMAGE_NAME:$3
+docker tag $URL/$IMAGE_NAME:$3 $IMAGE_NAME:$4
 
 docker compose down
+
 docker compose -p $IMAGE_NAME up -d
+
 docker compose ps
 
 docker image prune -a -f
