d59f7eff05073786c413fc26410b2caa806964e1
Joong Seok Hwang <hjs7949@naver.com>
2024-03-07 01:00:53 +0900

    [af-139] refactor: item 정보 수정

--- src/main/java/com/drunkenlion/alcoholfriday/domain/restaurant/dto/response/ItemResponse.java
+++ src/main/java/com/drunkenlion/alcoholfriday/domain/restaurant/dto/response/ItemResponse.java
@@ -1,22 +1,26 @@
 package com.drunkenlion.alcoholfriday.domain.restaurant.dto.response;
 
 import com.drunkenlion.alcoholfriday.domain.item.entity.Item;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import java.math.BigDecimal;
 
 @Getter
-@ToString
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ItemResponse {
 
+    @Schema(description = "상품에 대한 고유 아이디")
     private Long id;
 
+    @Schema(description = "상품의 이름")
     private String name;
 
+    @Schema(description = "상품의 가격")
     private BigDecimal price;
 
+    @Schema(description = "상품의 설명")
     private String info;
 
     public static ItemResponse of(Item item) {
