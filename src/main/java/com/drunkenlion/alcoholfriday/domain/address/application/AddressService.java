323038af4a7d8c546c07244529492ad0f75807bd
daseul-jang <devzzang94@gmail.com>
2024-03-08 17:37:04 +0900

    [af-105] feat: 배송지 수정 api 구현
    1. 배송지 수정 api 및 테스트 코드 구현
    2. MemberController 누락된 스웨거 추가

--- src/main/java/com/drunkenlion/alcoholfriday/domain/address/application/AddressService.java
+++ src/main/java/com/drunkenlion/alcoholfriday/domain/address/application/AddressService.java
@@ -1,6 +1,7 @@
 package com.drunkenlion.alcoholfriday.domain.address.application;
 
 import com.drunkenlion.alcoholfriday.domain.address.dto.AddressCreateRequest;
+import com.drunkenlion.alcoholfriday.domain.address.dto.AddressModifyRequest;
 import com.drunkenlion.alcoholfriday.domain.address.dto.AddressResponse;
 import com.drunkenlion.alcoholfriday.domain.member.entity.Member;
 
@@ -8,4 +9,6 @@ public interface AddressService {
     AddressResponse createAddress(Member member, AddressCreateRequest createRequest);
 
     AddressResponse getAddress(Long addressId);
+
+    AddressResponse modifyAddress(Long addressId, Long memberId, AddressModifyRequest modifyRequest);
 }
