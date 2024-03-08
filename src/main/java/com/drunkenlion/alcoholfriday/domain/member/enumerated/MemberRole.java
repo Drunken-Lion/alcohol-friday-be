029bfde662d4b14c36726adc9bad82081fc30d78
daseul-jang <devzzang94@gmail.com>
2024-03-09 00:43:13 +0900

    [af-145] refactor: ProviderType, MemberRole enum class 및 converter 변경
    1. MemberAPI 관련 enum class, converter 변경
    2. enum class 변경에 따른 테스트 코드 수정

--- src/main/java/com/drunkenlion/alcoholfriday/domain/member/enumerated/MemberRole.java
+++ src/main/java/com/drunkenlion/alcoholfriday/domain/member/enumerated/MemberRole.java
@@ -2,50 +2,60 @@ package com.drunkenlion.alcoholfriday.domain.member.enumerated;
 
 import com.drunkenlion.alcoholfriday.global.common.response.HttpResponse;
 import com.drunkenlion.alcoholfriday.global.exception.BusinessException;
+import lombok.Getter;
 
 import java.util.Arrays;
-
-import lombok.Getter;
 
 @Getter
 public enum MemberRole {
     /**
      * 일반 회원
      */
-    MEMBER("MEMBER"),
+    MEMBER("MEMBER", "5"),
 
     /**
      * 관리자
      */
-    ADMIN("ADMIN"),
+    ADMIN("ADMIN", "1"),
 
     /**
      * 슈퍼바이저 (인사관리)
      */
-    SUPER_VISOR("SUPER_VISOR"),
+    SUPER_VISOR("SUPER_VISOR", "2"),
 
     /**
      * 스토어 관리자
      */
-    STORE_MANAGER("STORE_MANAGER"),
+    STORE_MANAGER("STORE_MANAGER", "3"),
 
     /**
      * 레스토랑 사장
      */
-    OWNER("OWNER");
+    OWNER("OWNER", "4");
 
     private final String role;
+    private final String roleNumber;
 
-    MemberRole(String role) {
+    MemberRole(String role, String roleNumber) {
         this.role = role;
+        this.roleNumber = roleNumber;
     }
 
-    public static MemberRole ofRole(String role) {
+    public static MemberRole byRole(String role) {
         return Arrays.stream(MemberRole.values())
-                .filter(value -> value.role.equals(role))
+                .filter(value -> value.getRole().equals(role))
                 .findFirst()
                 .orElseThrow(() -> BusinessException.builder()
                         .response(HttpResponse.Fail.NOT_FOUND_ROLE)
+                        .build());
+    }
+
+    public static MemberRole byRoleNumber(String roleNumber) {
+        return Arrays.stream(MemberRole.values())
+                .filter(value -> value.getRoleNumber().equals(roleNumber))
+                .findAny()
+                .orElseThrow(() -> BusinessException.builder()
+                        .response(HttpResponse.Fail.NOT_FOUND_ROLE_NUMBER)
                         .build());
     }
 }
