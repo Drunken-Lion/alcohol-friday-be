abc87e8668556d42f22a675bea31389e476d8b84
tseop <77163842+seop-kim@users.noreply.github.com>
2024-03-08 17:39:02 +0900

    [af-181] refactor: 어노테이션 수정

--- src/main/java/com/drunkenlion/alcoholfriday/domain/customerservice/dto/request/QuestionModifyRequest.java
+++ src/main/java/com/drunkenlion/alcoholfriday/domain/customerservice/dto/request/QuestionModifyRequest.java
@@ -2,6 +2,7 @@ package com.drunkenlion.alcoholfriday.domain.customerservice.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.AccessLevel;
@@ -9,6 +10,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Getter
 @Builder
@@ -16,15 +18,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Schema(description = "문의사항 등록 요청 항목")
 public class QuestionModifyRequest {
-    @NotBlank(message = "문의의 제목이 존재하지 않습니다.")
     @Schema(description = "문의사항 제목")
     private String updateTitle;
 
-    @NotBlank(message = "문의의 내용이 존재하지 않습니다.")
     @Schema(description = "문의사항 내용")
     private String updateContent;
 
-    @Builder.Default
     @Schema(description = "삭제할 이미지 seq list")
-    private List<Integer> removeImageSeqList = new ArrayList<>();
+    private List<Integer> removeImageSeqList;
 }
