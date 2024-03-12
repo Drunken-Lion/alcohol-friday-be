a68e4f284c2b7c3d54fa8637d6f3d01f665075e4
daseul-jang <devzzang94@gmail.com>
2024-03-12 17:22:41 +0900

    [af-204] feat: RestaurantOrderCart, RestaurantOrderCartDetail entity 추가

---
+++ src/main/java/com/drunkenlion/alcoholfriday/domain/restaurant/cart/entity/RestaurantOrderCart.java
@@ -0,0 +1,26 @@
+package com.drunkenlion.alcoholfriday.domain.restaurant.cart.entity;
+
+import com.drunkenlion.alcoholfriday.domain.member.entity.Member;
+import com.drunkenlion.alcoholfriday.global.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@SuperBuilder(toBuilder = true)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "restaurant_order_cart")
+public class RestaurantOrderCart extends BaseEntity {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", columnDefinition = "BIGINT", foreignKey = @ForeignKey(value = ConstraintMode.NO_CONSTRAINT))
+    private Member member;
+
+    @OneToMany(mappedBy = "restaurantOrderCart")
+    @Builder.Default
+    private List<RestaurantOrderCartDetail> restaurantDetailOrders = new ArrayList<>();
+}
