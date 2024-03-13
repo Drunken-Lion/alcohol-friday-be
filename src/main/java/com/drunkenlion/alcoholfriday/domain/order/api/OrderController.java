3cc6f188ba3be51abea00a4108aa7870b9731c74
Son-Gyeongi <thsruddl77@gmail.com>
2024-03-13 23:30:30 +0900

    [af-63] conflict: dev 브랜치 rebase할 때 생긴 충돌 해결

--- src/main/java/com/drunkenlion/alcoholfriday/domain/order/api/OrderController.java
+++ src/main/java/com/drunkenlion/alcoholfriday/domain/order/api/OrderController.java
@@ -1,14 +1,21 @@
 package com.drunkenlion.alcoholfriday.domain.order.api;
 
 import com.drunkenlion.alcoholfriday.domain.order.application.OrderService;
+import com.drunkenlion.alcoholfriday.domain.order.dto.request.OrderRequestList;
+import com.drunkenlion.alcoholfriday.domain.order.dto.response.OrderResponseList;
 import com.drunkenlion.alcoholfriday.global.security.auth.UserPrincipal;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,7 +26,17 @@ public class OrderController {
 
     @Operation(summary = "주문 접수", description = "상품 바로 주문 또는 장바구니에서 주문할 때")
     @PostMapping
-    public void receive(@AuthenticationPrincipal UserPrincipal userPrincipal) {
+    public ResponseEntity<OrderResponseList> receive(
+            @RequestBody OrderRequestList orderRequestList,
+            @AuthenticationPrincipal UserPrincipal userPrincipal
+    ) {
+        OrderResponseList savedOrder = orderService.receive(orderRequestList, userPrincipal.getMember());
 
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedOrder.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(savedOrder);
     }
 }
--- src/main/java/com/drunkenlion/alcoholfriday/domain/order/application/OrderServiceImpl.java
+++ src/main/java/com/drunkenlion/alcoholfriday/domain/order/application/OrderServiceImpl.java
@@ -1,15 +1,80 @@
 package com.drunkenlion.alcoholfriday.domain.order.application;
 
+import com.drunkenlion.alcoholfriday.domain.item.dao.ItemRepository;
+import com.drunkenlion.alcoholfriday.domain.item.entity.Item;
+import com.drunkenlion.alcoholfriday.domain.member.entity.Member;
+import com.drunkenlion.alcoholfriday.domain.order.dao.OrderDetailRepository;
 import com.drunkenlion.alcoholfriday.domain.order.dao.OrderRepository;
+import com.drunkenlion.alcoholfriday.domain.order.dto.request.OrderItemRequest;
+import com.drunkenlion.alcoholfriday.domain.order.dto.request.OrderRequestList;
+import com.drunkenlion.alcoholfriday.domain.order.dto.response.OrderResponseList;
+import com.drunkenlion.alcoholfriday.domain.order.entity.Order;
+import com.drunkenlion.alcoholfriday.domain.order.entity.OrderDetail;
+import com.drunkenlion.alcoholfriday.global.common.enumerated.OrderStatus;
+import com.drunkenlion.alcoholfriday.global.common.response.HttpResponse;
+import com.drunkenlion.alcoholfriday.global.exception.BusinessException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 @Service
-@Transactional(readOnly = true)
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class OrderServiceImpl implements OrderService {
     private final OrderRepository orderRepository;
+    private final OrderDetailRepository orderDetailRepository;
+    private final ItemRepository itemRepository;
 
+    @Override
+    @Transactional
+    public OrderResponseList receive(OrderRequestList orderRequestList, Member member) {
+        // TODO 주문 고유번호 만들기
+        // TODO 주문 총금액은 프론트에서 주는건가? -> 서버에서도 한번 검사해야 할 텐데
 
+        Order order = Order.builder()
+//                .orderNo()
+                .orderStatus(OrderStatus.ORDER_RECEIVED)
+                .price(new BigDecimal("0"))
+                .recipient(orderRequestList.getRecipient())
+                .phone(orderRequestList.getPhone())
+                .address(orderRequestList.getAddress())
+                .detail(orderRequestList.getDetail())
+                .description(orderRequestList.getDescription())
+                .postcode(orderRequestList.getPostcode())
+                .member(member)
+                .build();
+
+        Order savedOrder = orderRepository.save(order);
+
+        List<OrderDetail> orderDetailList = orderRequestList.getOrderItemList().stream()
+                .map(orderItemRequest -> orderDetailSave(orderItemRequest, savedOrder))
+                .toList();
+
+        savedOrder.addPrice(orderDetailList);
+
+        return OrderResponseList.of(savedOrder, orderDetailList);
+    }
+
+    private OrderDetail orderDetailSave(OrderItemRequest orderItemRequest, Order order) {
+        Item item = itemRepository.findById(orderItemRequest.getItemId()).orElseThrow(() -> BusinessException.builder()
+                .response(HttpResponse.Fail.NOT_FOUND_ITEM).build());
+
+        // Long 타입의 quantity를 BigDecimal로 변환
+        BigDecimal quantityBigDecimal = BigDecimal.valueOf(orderItemRequest.getQuantity());
+        // BigDecimal 타입의 price와 BigDecimal 타입의 quantity를 곱하기
+        BigDecimal totalPrice = quantityBigDecimal.multiply(item.getPrice());
+
+        OrderDetail orderDetail = OrderDetail.builder()
+                .itemPrice(item.getPrice())
+                .quantity(orderItemRequest.getQuantity())
+                .totalPrice(totalPrice)
+                .item(item)
+                .order(order)
+                .build();
+
+        return orderDetailRepository.save(orderDetail);
+    }
 }
