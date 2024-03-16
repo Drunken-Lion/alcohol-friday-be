40b82b5c732176daedeb1b9a9935be06fa1768b8
Son-Gyeongi <thsruddl77@gmail.com>
2024-03-16 20:49:14 +0900

    [af-63] feat: 결제 버튼을 클릭했을 때 Product 재고 소진 진행중 - Order 테이블에 Member 연관관계메서드 안되는 현상 있음

--- src/main/java/com/drunkenlion/alcoholfriday/domain/order/application/OrderServiceImpl.java
+++ src/main/java/com/drunkenlion/alcoholfriday/domain/order/application/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package com.drunkenlion.alcoholfriday.domain.order.application;
 
 import com.drunkenlion.alcoholfriday.domain.item.dao.ItemRepository;
 import com.drunkenlion.alcoholfriday.domain.item.entity.Item;
+import com.drunkenlion.alcoholfriday.domain.item.entity.ItemProduct;
 import com.drunkenlion.alcoholfriday.domain.member.entity.Member;
 import com.drunkenlion.alcoholfriday.domain.order.dao.OrderDetailRepository;
 import com.drunkenlion.alcoholfriday.domain.order.dao.OrderRepository;
@@ -43,8 +44,9 @@ public class OrderServiceImpl implements OrderService {
                 .addressDetail(orderRequestList.getAddressDetail())
                 .description(orderRequestList.getDescription())
                 .postcode(orderRequestList.getPostcode())
+                .member(member)
                 .build();
-        order.addMember(member);
+//        order.addMember(member);
 
         Order savedOrder = orderRepository.save(order);
 
@@ -78,13 +80,41 @@ public class OrderServiceImpl implements OrderService {
         orderDetail.addItem(item);
         orderDetail.addOrder(order);
 
+        // 재고 줄이기
+        // 리스트로 만들어서 각각의 Product 재고 빼기
+        List<ItemProduct> itemProducts = item.getItemProducts();
+
+        /*for (ItemProduct ip : itemProducts) {
+            System.out.println("ip.getProduct().getQuantity() = " + ip.getProduct().getQuantity());
+        }
+        itemProducts.forEach(itemProduct -> updateProductQuantity(orderItemRequest.getQuantity(), itemProduct));
+
+        *//*for (ItemProduct itemProduct : itemProducts) {
+            System.out.println(itemProduct.getProduct().getId());
+            Product product = itemProduct.getProduct();
+            Long itemQuantity = itemProduct.getQuantity(); // item에 따른 개수
+            product.updateQuantity(orderItemRequest.getQuantity() * itemQuantity);
+        }*//*
+
+        for (ItemProduct ip : itemProducts) {
+            System.out.println("ip.getProduct().getQuantity()222 = " + ip.getProduct().getQuantity());
+        }*/
+
         return orderDetailRepository.save(orderDetail);
     }
 
-    private static BigDecimal getTotalItemPrice(OrderItemRequest orderItemRequest, Item item) {
+    public BigDecimal getTotalItemPrice(OrderItemRequest orderItemRequest, Item item) {
         // Long 타입의 quantity를 BigDecimal로 변환
         BigDecimal quantityBigDecimal = BigDecimal.valueOf(orderItemRequest.getQuantity());
         // BigDecimal 타입의 price와 BigDecimal 타입의 quantity를 곱하기
         return quantityBigDecimal.multiply(item.getPrice());
     }
+
+    /*@Transactional
+    public void updateProductQuantity(Long orderItemRequestQuantity, ItemProduct itemProduct) {
+        // TODO itemProduct, Product가 없을 수 있을까? 무슨 상황이든 예외는 처리해야겠지?
+        Product product = itemProduct.getProduct();
+        Long itemQuantity = itemProduct.getQuantity(); // item에 따른 개수
+        product.updateQuantity(orderItemRequestQuantity * itemQuantity);
+    }*/
 }
