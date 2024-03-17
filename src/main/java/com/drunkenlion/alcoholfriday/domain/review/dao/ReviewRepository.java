bff247d24947e3eb3bb44bdebc502ceef119ad8e
Son-Gyeongi <thsruddl77@gmail.com>
2024-03-17 18:09:23 +0900

    [af-295] feat: 한 개 item 조회 시 리뷰도 포함해서 응답 - 리뷰가 있을 때와 리뷰가 없을 때 테스트 완료

--- src/main/java/com/drunkenlion/alcoholfriday/domain/review/dao/ReviewRepository.java
+++ src/main/java/com/drunkenlion/alcoholfriday/domain/review/dao/ReviewRepository.java
@@ -5,6 +5,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     Page<Review> findAllByMemberIdAndDeletedAtIsNull(Long memberId, Pageable pageable);
+
+    List<Review> findAllByItemIdAndDeletedAtIsNull(Long itemId);
 }
