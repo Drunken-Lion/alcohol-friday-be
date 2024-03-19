b76a800be9a9572cdc8f7f0ed359dbff29ba691c
eonjuseo <sksjang12@gmail.com>
2024-03-19 18:16:20 +0900

    [af-302] refactor: 공지사항 목록 queryDSL 적용
    공지사항 목록을 가져오는 jpql을 queryDSL로 변경했습니다.

--- src/main/java/com/drunkenlion/alcoholfriday/domain/customerservice/notice/dao/NoticeRepository.java
+++ src/main/java/com/drunkenlion/alcoholfriday/domain/customerservice/notice/dao/NoticeRepository.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface NoticeRepository extends JpaRepository<Notice, Long> {
+public interface NoticeRepository extends JpaRepository<Notice, Long>, NoticeCustomRepository {
 
     Optional<Notice> findByIdAndDeletedAtIsNull(Long id);
 
