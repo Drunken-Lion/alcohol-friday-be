a0310fe579c3e91dcb5007e9e119e599e3de6d7a
Son-Gyeongi <thsruddl77@gmail.com>
2024-03-19 14:44:44 +0900

    [af-64] feat: 토스페이먼츠 참고해서 결제 PaymentController 작성

---
+++ src/main/java/com/drunkenlion/alcoholfriday/domain/payment/api/PaymentController.java
@@ -0,0 +1,91 @@
+package com.drunkenlion.alcoholfriday.domain.payment.api;
+
+import com.drunkenlion.alcoholfriday.domain.payment.application.PaymentService;
+import com.drunkenlion.alcoholfriday.domain.payment.dto.request.TossPaymentsReq;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/payments")
+@Tag(name = "v1-payment", description = "결제 관련 API")
+public class PaymentController {
+    private final PaymentService paymentService;
+
+    @Value("${custom.tossPayments.widget.secretKey}")
+    private String tossPaymentsWidgetSecretKey;
+
+    @Operation(summary = "결제",
+            description = "클라이언트에서 결제 정보를 전달 받고 서버에서 한 번 더 검사 후 토스페이먼츠로 최종 결제 승인 요청")
+    @PostMapping("confirm")
+    public ResponseEntity<JSONObject> confirmPayment(@RequestBody String jsonBody) throws Exception {
+
+        JSONParser parser = new JSONParser();
+        String orderId;
+        String amount;
+        String paymentKey;
+        try {
+            JSONObject requestData = (JSONObject) parser.parse(jsonBody);
+            paymentKey = (String) requestData.get("paymentKey");
+            orderId = (String) requestData.get("orderId");
+            amount = (String) requestData.get("amount");
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        };
+
+        paymentService.checkAmountValidity(orderId, new BigDecimal(amount));
+
+        JSONObject obj = new JSONObject();
+        obj.put("orderId", orderId);
+        obj.put("amount", amount);
+        obj.put("paymentKey", paymentKey);
+
+        String widgetSecretKey = tossPaymentsWidgetSecretKey;
+        Base64.Encoder encoder = Base64.getEncoder();
+        byte[] encodedBytes = encoder.encode((widgetSecretKey + ":").getBytes("UTF-8"));
+        String authorizations = "Basic " + new String(encodedBytes, 0, encodedBytes.length);
+
+        URL url = new URL("https://api.tosspayments.com/v1/payments/confirm");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestProperty("Authorization", authorizations);
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setRequestMethod("POST");
+        connection.setDoOutput(true);
+
+        OutputStream outputStream = connection.getOutputStream();
+        outputStream.write(obj.toString().getBytes("UTF-8"));
+
+        int code = connection.getResponseCode();
+        boolean isSuccess = code == 200 ? true : false;
+
+        InputStream responseStream = isSuccess ? connection.getInputStream() : connection.getErrorStream();
+
+        Reader reader = new InputStreamReader(responseStream, StandardCharsets.UTF_8);
+        JSONObject jsonObject = (JSONObject) parser.parse(reader);
+        responseStream.close();
+
+        if (isSuccess) paymentService.saveSuccessPayment(TossPaymentsReq.of(orderId, paymentKey, jsonObject));
+
+        return ResponseEntity.status(code).body(jsonObject);
+    }
+}
