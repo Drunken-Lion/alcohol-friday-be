edef54cc70ddad24ea328444d4d4e1db40e27371
Son-Gyeongi <thsruddl77@gmail.com>
2024-03-24 18:12:29 +0900

    [af-317] feat: 주문 접수 API 요청, 응답 코드 수정 - 즉시 구매 또는 장바구니에서 구매할 때 사용 - 요청할 때 아이템의 id, 수량 요청 - 응답할 때 주문 상품, 구매자 정보, 배송 정보 응답

--- src/main/java/com/drunkenlion/alcoholfriday/domain/order/dto/response/OrderDetailResponse.java
+++ src/main/java/com/drunkenlion/alcoholfriday/domain/order/dto/response/OrderDetailResponse.java
@@ -5,6 +5,8 @@ import com.drunkenlion.alcoholfriday.domain.order.entity.OrderDetail;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
+import java.math.BigDecimal;
+
 @Getter
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -15,11 +17,14 @@ public class OrderDetailResponse {
     private FindItemResponse item;
     @Schema(description = "주문 상품 수량")
     private Long quantity;
+    @Schema(description = "총 상품 금액")
+    private BigDecimal totalPrice;
 
     public static OrderDetailResponse of(OrderDetail orderDetail) {
         return OrderDetailResponse.builder()
                 .item(FindItemResponse.of(orderDetail.getItem()))
                 .quantity(orderDetail.getQuantity())
+                .totalPrice(orderDetail.getTotalPrice())
                 .build();
     }
 }
