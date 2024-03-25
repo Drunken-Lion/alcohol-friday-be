872fe00ca85f4ead338fd5afd91ea45b4a81700e
daseul-jang <devzzang94@gmail.com>
2024-03-25 22:34:17 +0900

    [af-326] chore: admin restaurant dto 패키지 구조 변경

--- src/main/java/com/drunkenlion/alcoholfriday/domain/admin/restaurant/restaurant/dto/response/RestaurantListResponse.java
+++ src/main/java/com/drunkenlion/alcoholfriday/domain/admin/restaurant/restaurant/dto/response/RestaurantListResponse.java
@@ -1,4 +1,4 @@
-package com.drunkenlion.alcoholfriday.domain.admin.restaurant.restaurant.dto;
+package com.drunkenlion.alcoholfriday.domain.admin.restaurant.restaurant.dto.response;
 
 import com.drunkenlion.alcoholfriday.domain.restaurant.entity.Restaurant;
 import io.swagger.v3.oas.annotations.media.Schema;
