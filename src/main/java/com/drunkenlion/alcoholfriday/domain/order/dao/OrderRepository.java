3626eb17c4ac77248e5a10f70e19dcf178a809f0
Son-Gyeongi <thsruddl77@gmail.com>
2024-03-25 22:27:08 +0900

    [af-64] conflict: dev 브랜치 rebase할 때 생긴 충돌 해결2

--- src/main/java/com/drunkenlion/alcoholfriday/domain/order/dao/OrderRepository.java
+++ src/main/java/com/drunkenlion/alcoholfriday/domain/order/dao/OrderRepository.java
@@ -1,9 +1,6 @@
 package com.drunkenlion.alcoholfriday.domain.order.dao;
 
 import com.drunkenlion.alcoholfriday.domain.order.entity.Order;
-import com.drunkenlion.alcoholfriday.global.common.enumerated.OrderStatus;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
