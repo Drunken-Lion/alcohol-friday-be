84c45928d3bc34edfc68cc850ff0ea7a2cf260a5
eonjuseo <sksjang12@gmail.com>
2024-03-25 10:30:45 +0900

    [af-260] test: 레스토랑 발주 장바구니 목록 테스트 코드 작성

--- src/test/java/com/drunkenlion/alcoholfriday/domain/admin/restaurant/cart/application/RestaurantOrderCartServiceTest.java
+++ src/test/java/com/drunkenlion/alcoholfriday/domain/admin/restaurant/cart/application/RestaurantOrderCartServiceTest.java
@@ -3,6 +3,7 @@ package com.drunkenlion.alcoholfriday.domain.admin.restaurant.cart.application;
 import static com.drunkenlion.alcoholfriday.domain.admin.restaurant.cart.entity.QRestaurantOrderCartDetail.restaurantOrderCartDetail;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,9 +12,11 @@ import com.drunkenlion.alcoholfriday.domain.admin.restaurant.cart.dao.Restaurant
 import com.drunkenlion.alcoholfriday.domain.admin.restaurant.cart.dto.request.RestaurantOrderCartDeleteRequest;
 import com.drunkenlion.alcoholfriday.domain.admin.restaurant.cart.dto.request.RestaurantOrderCartSaveRequest;
 import com.drunkenlion.alcoholfriday.domain.admin.restaurant.cart.dto.request.RestaurantOrderCartUpdateRequest;
+import com.drunkenlion.alcoholfriday.domain.admin.restaurant.cart.dto.response.RestaurantOrderCartListResponse;
 import com.drunkenlion.alcoholfriday.domain.admin.restaurant.cart.dto.response.RestaurantOrderCartSaveResponse;
 import com.drunkenlion.alcoholfriday.domain.admin.restaurant.cart.entity.RestaurantOrderCart;
 import com.drunkenlion.alcoholfriday.domain.admin.restaurant.cart.entity.RestaurantOrderCartDetail;
+import com.drunkenlion.alcoholfriday.domain.admin.restaurant.restaurant.dto.RestaurantListResponse;
 import com.drunkenlion.alcoholfriday.domain.maker.entity.Maker;
 import com.drunkenlion.alcoholfriday.domain.member.entity.Member;
 import com.drunkenlion.alcoholfriday.domain.member.enumerated.MemberRole;
@@ -26,6 +29,7 @@ import com.drunkenlion.alcoholfriday.global.common.response.HttpResponse.Fail;
 import com.drunkenlion.alcoholfriday.global.exception.BusinessException;
 import com.drunkenlion.alcoholfriday.global.file.application.FileService;
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
@@ -35,6 +39,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 
 @ExtendWith(MockitoExtension.class)
@@ -511,5 +519,126 @@ class RestaurantOrderCartServiceTest {
         });
 
         assertThat(businessException.getStatus()).isEqualTo(Fail.FORBIDDEN.getStatus());
+    }
+
+    @Test
+    @DisplayName("Owner는 장바구니 목록 조회")
+    public void t10() {
+        Maker maker = Maker.builder()
+                .id(1L)
+                .name("테스트")
+                .build();
+
+        Product product = Product.builder()
+                .id(1L)
+                .name("테스트 제품")
+                .quantity(100L)
+                .distributionPrice(BigDecimal.valueOf(10000L))
+                .maker(maker)
+                .build();
+
+        Product product2 = Product.builder()
+                .id(2L)
+                .name("테스트 제품2")
+                .quantity(100L)
+                .distributionPrice(BigDecimal.valueOf(10000L))
+                .maker(maker)
+                .build();
+
+        Member member = Member.builder()
+                .id(1L)
+                .role(MemberRole.OWNER)
+                .build();
+
+        Restaurant restaurant = Restaurant.builder()
+                .id(1L)
+                .member(member)
+                .build();
+
+        RestaurantOrderCart restaurantOrderCart = RestaurantOrderCart.builder()
+                .id(1L)
+                .member(member)
+                .restaurant(restaurant)
+                .build();
+
+        RestaurantOrderCartDetail restaurantOrderCartDetail = RestaurantOrderCartDetail.builder()
+                .id(1L)
+                .product(product)
+                .quantity(10L)
+                .restaurantOrderCart(restaurantOrderCart)
+                .build();
+
+        RestaurantOrderCartDetail restaurantOrderCartDetail2 = RestaurantOrderCartDetail.builder()
+                .id(2L)
+                .product(product2)
+                .quantity(10L)
+                .restaurantOrderCart(restaurantOrderCart)
+                .build();
+
+        when(this.restaurantRepository.findByIdAndDeletedAtIsNull(restaurant.getId())).thenReturn(Optional.of(restaurant));
+        when(this.restaurantOrderCartRepository.findRestaurantAndMember(restaurant, member)).thenReturn(Optional.of(restaurantOrderCart));
+        when(this.restaurantOrderCartDetailRepository.findRestaurantAndMember(any(), any(), any()))
+                .thenReturn(getRestaurantsOrderCartDetails(RestaurantOrderCartDetail.builder()
+                        .id(1L)
+                        .product(product)
+                        .quantity(10L)
+                        .restaurantOrderCart(restaurantOrderCart)
+                        .build()
+                ));
+
+        Page<RestaurantOrderCartListResponse> restaurantOrderCartList = this.restaurantOrderCartService.findRestaurantCart(restaurant.getId(), member, 0, 20);
+
+        List<RestaurantOrderCartListResponse> content = restaurantOrderCartList.getContent();
+
+        assertThat(content).isInstanceOf(List.class);
+        assertThat(content.get(0).getId()).isEqualTo(product.getId());
+        assertThat(content.get(0).getMakerName()).isEqualTo(maker.getName());
+        assertThat(content.get(0).getPrice()).isEqualTo(product.getDistributionPrice());
+        assertThat(content.get(0).getQuantity()).isEqualTo(restaurantOrderCartDetail.getQuantity());
+    }
+
+    @Test
+    @DisplayName("Owner가 아니면 장바구니 목록 조회가 불가하다.")
+    public void t11() {
+        Maker maker = Maker.builder()
+                .id(1L)
+                .name("테스트")
+                .build();
+
+        Product product = Product.builder()
+                .id(1L)
+                .name("테스트 제품")
+                .quantity(100L)
+                .distributionPrice(BigDecimal.valueOf(10000L))
+                .maker(maker)
+                .build();
+
+        Member member = Member.builder()
+                .id(1L)
+                .role(MemberRole.MEMBER)
+                .build();
+
+        Restaurant restaurant = Restaurant.builder()
+                .id(1L)
+                .member(member)
+                .build();
+
+        RestaurantOrderCart restaurantOrderCart = RestaurantOrderCart.builder()
+                .id(1L)
+                .member(member)
+                .restaurant(restaurant)
+                .build();
+
+        BusinessException businessException = assertThrows(BusinessException.class, () -> {
+            restaurantOrderCartService.findRestaurantCart(restaurant.getId(), member, 0, 20);
+        });
+
+        assertThat(businessException.getStatus()).isEqualTo(Fail.FORBIDDEN.getStatus());
+    }
+
+    private Page<RestaurantOrderCartDetail> getRestaurantsOrderCartDetails(RestaurantOrderCartDetail restaurantOrderCart) {
+        List<RestaurantOrderCartDetail> list = List.of(restaurantOrderCart);
+        Pageable pageable = PageRequest.of(0, 20);
+        return new PageImpl<RestaurantOrderCartDetail>(list, pageable, list.size());
     }
 }
