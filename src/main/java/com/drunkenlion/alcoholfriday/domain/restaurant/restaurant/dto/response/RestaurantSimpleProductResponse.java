1e830383062e0fcd332defeb9ae790826d99ffe6
seop <77163842+seop-kim@users.noreply.github.com>
2024-03-25 22:11:41 +0900

    [af-322] feat: response 객체 내 스키마 삽입

--- src/main/java/com/drunkenlion/alcoholfriday/domain/restaurant/restaurant/dto/response/RestaurantSimpleProductResponse.java
+++ src/main/java/com/drunkenlion/alcoholfriday/domain/restaurant/restaurant/dto/response/RestaurantSimpleProductResponse.java
@@ -15,8 +15,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Schema(description = "매장 취급 주류 간략 정보 반환 객체")
 public class RestaurantSimpleProductResponse {
+    @Schema(description = "제품 고유 식별 ID")
     private Long id;
+
+    @Schema(description = "제품 이름")
     private String name;
+
+    @Schema(description = "제품 이미지")
     private NcpFileResponse files;
 
     public static RestaurantSimpleProductResponse of(RestaurantStock restaurantStock, NcpFileResponse files) {
