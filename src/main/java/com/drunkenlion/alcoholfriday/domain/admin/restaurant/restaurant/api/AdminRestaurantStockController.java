e59a0e93b326646a87011efdc04ac7a323ca0997
daseul-jang <devzzang94@gmail.com>
2024-03-26 14:05:15 +0900

    [af-326] style: 누락된 @SecurityRequirement 추가

--- src/main/java/com/drunkenlion/alcoholfriday/domain/admin/restaurant/restaurant/api/AdminRestaurantStockController.java
+++ src/main/java/com/drunkenlion/alcoholfriday/domain/admin/restaurant/restaurant/api/AdminRestaurantStockController.java
@@ -6,6 +6,7 @@ import com.drunkenlion.alcoholfriday.global.common.response.PageResponse;
 import com.drunkenlion.alcoholfriday.global.common.util.RoleValidator;
 import com.drunkenlion.alcoholfriday.global.security.auth.UserPrincipal;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/v1/admin/restaurants/{restaurantId}/stocks")
 @Tag(name = "v1-admin-restaurant-{restaurantId}-stocks", description = "관리자 매장 재고 관리 API")
+@SecurityRequirement(name = "bearerAuth")
 public class AdminRestaurantStockController {
     private final AdminRestaurantStockService adminRestaurantStockService;
 
